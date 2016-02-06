53c9df1f8c1b024b620691f8f56aec2adf667ba7
Matt <mparman6@yahoo.com>
2016-02-06 11:06:12 -0500

    added validate.js

---
+++ public/js/validate.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#registrationForm").on("submit", function(e) {
+    if($("#registerEmail").val().length === 0) {
+      alert("Please enter an email");
+      return false;
+    } else if($("#registerPassword").val().length === 0) {
+      alert("Please enter a password");
+      return false;
+    } else {
+      return true;
+    }
+  });
+});
